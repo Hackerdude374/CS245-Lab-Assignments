61a01958a8b51fa7220bd02a830a943f416a95d4
Hackerdude374 <robertle982@gmail.com>
2024-03-05 19:02:11 -0500

    reorganized files

--- test.py
+++
@@ -1,7 +0,0 @@
-# #1. Create a rank 2 (2D) array that resembles the matrix below.
-# [[11 12 13 14]
-#  [15 16 17 18]]
-import numpy as np # import np 
-print ("hello world ") # test working python
-twoDarray = np.array([[11,12,13,14],[15,16,17,18]])
-print(twoDarray)
